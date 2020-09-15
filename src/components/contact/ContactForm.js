39f0365f0805757a0612b9c8a9bef7fe5493b712
pratap <pratapadhikari37@gmail.com>
2020-09-16 09:28:12 +0930

    form done connect to netlify

--- src/components/contact/ContactForm.js
+++ src/components/contact/ContactForm.js
@@ -1,49 +1,21 @@
 import React from "react";
 import { Button, FormContainer } from "./ContactForm.style";
 const ContactForm = () => {
-	/* const [name, setName] = useState(" ");
-	const [email, setEmail] = useState(" ");
-	const [text, setText] = useState(" ");
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		console.log(name, email, text);
-	};
- */
 	return (
 		<FormContainer>
 			<p> Have a question or want to work together?</p>
-			<form name="contact" method="post">
-				<input type="hidden" name="form-name" value="contact" />
+			<form name="contact" action="/contact" method="post">
+				<input type="hidden" name="from-name" value="contact" />
 				<div>
-					<input
-						type="text"
-						placeholder=" Name"
-						/* 	value={name}
-						onChange={(e) => setName(e.target.value)} */
-					/>
+					<input required type="text" placeholder=" Name" />
 				</div>
 				<div>
-					<input
-						type="email"
-						placeholder="Enter email"
-						/* value={email}
-						onChange={(e) => setEmail(e.target.value)} */
-					/>
+					<input required type="email" placeholder="Enter email" />
 				</div>
 
 				<div>
-					<textarea
-						type="textarea"
-						placeholder="Your Message"
-
-						/* value={text}
-						onChange={(e) => setText(e.target.value)} */
-					/>
+					<textarea required type="textarea" placeholder="Your Message" />
 				</div>
-				{/* <div>
-					<div data-netlify-recaptcha="true"></div>
-				</div> */}
 			</form>
 			<Button type="button" className="submit">
 				<span>Send Message </span>
