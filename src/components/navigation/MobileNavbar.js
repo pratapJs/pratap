ef3ab19506a96169fb707219a61ea8e9fa937962
pratap <pratapadhikari37@gmail.com>
2020-09-16 15:46:01 +0930

    toggle navbar updated

--- src/components/navigation/MobileNavbar.js
+++ src/components/navigation/MobileNavbar.js
@@ -19,20 +19,22 @@ export default MobileNavbar;
 const MyMobileNavBar = styled.nav`
 	/* clip-path: polygon(100% 0%, 100% 0%, 0 100%, 0 0); */
 	background-color: ${setColor.primaryColor};
+	display: none;
 
 	/* visibility: ${(props) => !props.displayMobileNavBar && "hidden"}; */
-	/* display: ${(props) => props.displayMobileNavBar && "block"}; */
+	display: ${(props) => props.displayMobileNavBar && "block"};
 
 	overflow: hidden;
 	position: absolute;
 	margin-top: 6.8rem;
 	width: 100%;
 	align-self: flex-end;
-	transition: transform 0.5s ease-in-out;
+	/* transition: transform 0.5s ease-in-out;
 
 	transform: translateX(
-		${(props) => (props.displayMobileNavBar ? "0%" : "100%")}
-	);
+		${(props) =>
+		props.displayMobileNavBar ? "0%" : "100%"}
+	); */
 	.nav-links {
 		display: flex;
 		flex-flow: column nowrap;
