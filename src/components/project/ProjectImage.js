583f56a5caf9a31903549354221bfe1d76a4bee3
Pratap <pratapadhikari81@gmail.com>
2025-12-27 09:49:51 +0930

    Fix deprecation warning, upgrade react-scripts, and fix compilation errors

--- src/components/project/ProjectImage.js
+++ src/components/project/ProjectImage.js
@@ -1,138 +1,37 @@
 import React from "react";
 import styled from "styled-components";
 import { media } from "../../styles";
-import todoapp from "../../images/todoapp.png";
-import cocktails from "../../images/cocktails.png";
-import beachresort from "../../images/resortwebsite.png";
-import speech from "../../images/speech recognition.png";
-import exchangerate from "../../images/exchange rate.png";
-import relaxer from "../../images/relaxer.png";
-import feedbackapp from "../../images/feedbackapp.png";
-import githubuser from "../../images/githubuser.png";
+
 import ecommerce from "../../images/eCommerce.png";
+import jekamobile from "../../images/jekamobile.png";
 
 const ProjectImage = () => {
 	return (
 		<ImageContainer>
 			<div className="imagewrapper">
 				<p>
-					Complete eCommerce Site <br />
-					&ndash; Complete eCommerce site using React, Redux, ExpressJs
+					Ecommerce Website <br />
+					&ndash; Complete e-commerce site using React, Redux, ExpressJs
 				</p>
 				<a
 					href="http://104.131.15.211/"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<img src={ecommerce} alt="eCommerce app" />
+					<img src={ecommerce} alt="store app" />
 				</a>
 			</div>
 			<div className="imagewrapper">
 				<p>
-					Search Github User <br />
-					&ndash; search github users, popular languages, followers
+					Mobile Store and Repair shop <br />
+					&ndash; Complete store site using React, Redux, ExpressJs
 				</p>
 				<a
-					href="https://searchgitubuser.netlify.app/"
+					href="https://mobilestoreandrepairshop.netlify.app/"
 					target="_blank"
 					rel="noopener noreferrer"
 				>
-					<img src={githubuser} alt="searchgithubuser app" />
-				</a>
-			</div>
-			<div className="imagewrapper">
-				<p>
-					{" "}
-					demo hotel website <br />
-					&ndash; includes all the essential features
-				</p>
-				<a
-					href="https://resortwebsitedemo.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={beachresort} alt="resort website app" />{" "}
-				</a>
-			</div>
-			<div className="imagewrapper">
-				<p>
-					{" "}
-					Cocktails Training demo website <br />
-					&ndash; can search for favourite cocktail drink <br />
-					and check the ingredients too
-				</p>
-				<a
-					href="https://cocktailsbypratap.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={cocktails} alt="cocktail app" />{" "}
-				</a>{" "}
-			</div>
-			<div className="imagewrapper">
-				<p>
-					Speech recognition app <br /> &ndash; speak and guess the number
-					between 1 to 100
-				</p>
-				<a
-					href="https://speechrecognitionguess.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={speech} alt="speech recognition app" />
-				</a>
-			</div>
-			<div className="imagewrapper">
-				<p>
-					Currency exchange rate <br />
-					&ndash; check the current currency exchange rate
-				</p>
-				<a
-					href="https://exchangeratecurrency.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={exchangerate} alt="todo app" />
-				</a>
-			</div>
-			<div className="imagewrapper">
-				<p>
-					Relax your mind <br />
-					&ndash; watch the screen to relax your mind
-				</p>
-				<a
-					href="https://relaxermindapp.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={relaxer} alt="relaxer app" />
-				</a>
-			</div>
-			<div className="imagewrapper">
-				<p>
-					Business Feedback App <br />
-					&ndash; Email your feedback
-				</p>
-				<a
-					href="https://react-local-businesses.herokuapp.com/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={feedbackapp} alt="feedback app" />
-				</a>
-			</div>
-
-			<div className="imagewrapper">
-				<p>
-					Todo app <br />
-					&ndash; add ,delete and update your todo jobs
-				</p>
-				<a
-					href="https://prataptodos.netlify.app/"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<img src={todoapp} alt="todo app" />
+					<img src={jekamobile} alt="store app" />
 				</a>
 			</div>
 		</ImageContainer>
