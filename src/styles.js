49c0091450241b78f705a30694c8fced153e4d7b
Pratap <pratapadhikari81@gmail.com>
2025-12-27 10:21:20 +0930

    Fix Circular Dependency: Move Buttons to styles.js

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,4 @@
-import { keyframes, css } from "styled-components";
-import { mainButton as Button } from "./globalStyles/globals/Buttons";
-export { Button };
+import styled, { keyframes, css } from "styled-components";
 
 const sizes = {
 	mobileS: 320,
@@ -82,3 +80,34 @@ export const setTransition = ({
 } = {}) => {
 	return `transition:${property} ${time} ${timing}`;
 };
+
+export const mainButton = styled.button`
+	display: inline-block;
+	text-decoration: none;
+	padding: 1rem;
+	text-align: center;
+	cursor: pointer;
+
+	background: transparent;
+	color: ${(props) => (props.color ? props.color : "white")};
+	${(props) => setBorder({ width: "2px", style: "solid" })};
+	border-color: ${(props) => (props.color ? props.color : "white")};
+	font-size: 2.5rem;
+	text-transform: capitalize;
+
+	span {
+		color: #fff;
+	}
+	&:hover {
+		background-color: ${setColor.redColor};
+		padding-right: 0.5em;
+		transition: all 0.2s ease-in-out;
+	}
+`;
+
+export const SmallBtn = styled(mainButton)`
+	font-size: 1.5rem;
+	padding: 0.2rem 1rem;
+`;
+
+export const Button = mainButton;
